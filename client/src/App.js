b5b492e0fed7e888d2a86ac0e19d3c2fe70ff3ae
Alberto Galvan <astrometrics@Unimatrix-Zero.local>
2019-11-01 11:24:09 -0500

    added hook and component files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  state = {
+    api: 'http://localhost:5000/api/players',
+    players: [],
+  };
+
+  componentDidMount() {
+    axios.get(this.state.api)
+      .then(response => {
+        const list = response.data;
+        console.log('got the data', response.data)
+        this.setState({ players: list })
+      })
+      .catch(error => console.log('nope on the data'));
+
+  }
+  render() {
+    return (
+      <div className="App" >
+
+      </div>
+    );
+  }
 }
 
 export default App;
